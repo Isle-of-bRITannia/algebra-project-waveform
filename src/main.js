76b411c5674f2b1ebdb1d0478211ac4e658fd058
chigbe001 <chigbe001@gmail.com>
2023-04-19 15:14:19 -0400

    src update still

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import * as canvas from "./canvas.js";
-import { Wave } from './waveApi.js';
-import * as audio from "./audio.js";
+import * as canvas from "./canvas/canvas.js";
+import { Wave } from './waveAPI/waveApi.js';
+import * as audio from "./audio/audio.js";
 let waves = [];
 waves[0] = Wave.sinesoidal(1,1);
 waves[1] = Wave.combine(Wave.sinesoidal(1,1), Wave.sinesoidal(8,2));
-let myNewWave = Wave.sinesoidal(1,.00001);//Wave.synthesize(waves);
+let myNewWave = Wave.sinesoidal(1,10);//Wave.synthesize(waves);
 //window.onload(() => {
     //console.log('loaded');
     canvas.drawWave(myNewWave);
 //});
-const mySound = new Float32Array(audio.sampleWave(220,myNewWave));
+let mySound; 
 const playButton = document.getElementById("playButton");
 
-playButton.onclick = () => {
-    
+playButton.onclick = () => {    
+    mySound = new Float32Array(audio.sampleWave(220,myNewWave));
     audio.playSound({array: mySound,samplerate: 44100});
 };

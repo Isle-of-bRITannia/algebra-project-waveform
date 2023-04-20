f28a6eec0b89ecff54d47359bc916a2b50406b67
Abraham Furlan <abrahamfurlan@gmail.com>
2023-04-19 21:19:43 -0400

    made cards

--- src/main.js
+++ src/main.js
@@ -4,30 +4,37 @@ import * as audio from './audio/audio.js';
 import * as card from './waveCard/card.js';
 let waves = [];
 let myFinishedWave;
-waves[0] = Wave.sinesoidal(1,1);
-waves[1] = Wave.synthesize([Wave.sinesoidal(1,1), Wave.sinesoidal(8,2)]);
+waves[0] = Wave.sinesoidal(1, 1);
+waves[1] = Wave.synthesize([Wave.sinesoidal(1, 1), Wave.sinesoidal(8, 2)]);
 
 //window.onload(() => {
-    //console.log('loaded');
-    canvas.drawWave(myNewWave);
+//console.log('loaded');
+//canvas.drawWave(myNewWave);
 //});
-let mySound; 
+
+let mySound;
 const playButton = document.getElementById("playButton");
 const synthesizeButton = document.getElementById("synthButton");
 const newButton = document.getElementById("newButton");
 
-playButton.onclick = () => {    
-    mySound = new Float32Array(audio.sampleWave(220,myNewWave));
-    audio.playSound({array: mySound,samplerate: 44100});
+playButton.onclick = () => {
+    mySound = new Float32Array(audio.sampleWave(220, myNewWave));
+    audio.playSound({ array: mySound, samplerate: 44100 });
 };
 
 synthesizeButton.onclick = () => {
+    // collect the info from each wave card in the waves array
     myFinishedWave = Wave.synthesize(waves);
 }
 
-function createCard(){
+newButton.onclick = () => {
+    const container = document.querySelector('.container');
+    container.appendChild(createCard());
+}
+
+function createCard() {
     const newCard = document.createElement("wave-card");
-    newCard.dataset.waveType = "Sinesoidal";
+    newCard.dataset.waveType = "sine";
     newCard.dataset.period = 1;
     newCard.dataset.skew = 10;
     newCard.dataset.amplitude = 1;
@@ -36,4 +43,5 @@ function createCard(){
     newCard.dataset.clampVal = 1;
     newCard.dataset.invert = false;
     newCard.dataset.select = false;
+    return newCard;
 }
--- src/waveCard/card.js
+++ src/waveCard/card.js
@@ -3,74 +3,123 @@ const template = document.createElement("template");
 
 template.innerHTML = `
     <div class="box">
-        <h1></h1>
+        <button>Remove</button>
 
-        <button></button>
+        <span>
+            <label for="waveType">WaveType</label>
+            <select name="waveType" id="waves">
+                <option value="sine">Sinesoidal</option>
+                <option value="rect">Rectangle</option>
+                <option value="tri">Triangle</option>
+                <option value="saw">Sawtooth</option>
+            </select>
+        </span>
 
-        <label for="waveType">WaveType</label>
-        <select name="waveType" id="cars">
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
-        </select>
 
         <span class="input">
-            <p>Invert</p>
-            <input type="checkbox">
+            <label for="invert">Invert</label>
+            <input id="invert" name="invert" type="checkbox">
+        </span>
+
+        <span class="input">
+            <label for="clamp">Clamp</label>
+            <input id="clamp" name="clamp" type="checkbox">
+        </span>
+
+        <span class="input">
+            <p>Clamp Value</p>
+            <input id="clamp-range" type="range">
         </span>
 
         <span class="input">
             <p>Amplitude</p>
-            <input type="range">
+            <input id="amplitude" type="range">
         </span>
 
         <span class="input">
             <p>Period</p>
-            <input type="range">
+            <input id="period" type="range">
         </span>
 
         <span class="input">
             <p>Skew</p>
-            <input type="range">
+            <input id="skew" type="range">
         </span>
 
         <span class="input">
             <p>Shift</p>
-            <input type="range">
+            <input id="shift" type="range">
         </span>
 
-
     </div>
+    <hr>
 `;
 
-class Card extends HTMLElement{
-    constructor(){
+class Card extends HTMLElement {
+    constructor() {
         super();
-        this.attachShadow({mode: "open"});
+        this.attachShadow({ mode: "open" });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this.h1 = this.shadowRoot.querySelector("h1");
-        this.button = this.shadowRoot.querySelector("button");
+        // this.h1 = this.shadowRoot.querySelector("h1");
+        this.removeButton = this.shadowRoot.querySelector("button");
+        this.waveType = this.shadowRoot.querySelector("#waves");
+        this.invert = this.shadowRoot.querySelector("#invert");
+        this.clamp = this.shadowRoot.querySelector("#clamp");
+        this.clampRange = this.shadowRoot.querySelector("#clamp-range");
+        this.amplitude = this.shadowRoot.querySelector("#amplitude");
+        this.period = this.shadowRoot.querySelector("#period");
+        this.skew = this.shadowRoot.querySelector("#skew");
+        this.shift = this.shadowRoot.querySelector("#shift");
+
     }
-    
-    connectedCallback(){
-        this.button.innerHTML = "Favorite"
-        this.button.onclick = () => {
-            storage.addFavorite(this.getAttribute('data-id'));
-            this.button.innerHTML = "Favorited"
-            this.button.disabled = true;
-            console.log(storage.readLocalStorage().favorites);
+
+    connectedCallback() {
+        this.removeButton.innerHTML = "Remove"
+        this.removeButton.onclick = () => {
+            this.remove();
+        }
+        this.waveType.onclick = () => {
+            this.setAttribute('data-wave-type', this.waveType.value);
+        }
+        this.invert.onclick = () => {
+            this.setAttribute('data-invert', this.invert.checked);
+        }
+        this.clamp.onclick = () => {
+            this.setAttribute('data-clamp', this.clamp.checked);
+        }
+        this.clampRange.onclick = () => {
+            this.setAttribute('data-clamp-val', this.clampRange.value);
+        }
+        this.amplitude.onclick = () => {
+            this.setAttribute('data-amplitude', this.amplitude.value);
+        }
+        this.period.onclick = () => {
+            this.setAttribute('data-period', this.period.value);
+        }
+        this.skew.onclick = () => {
+            this.setAttribute('data-skew', this.skew.value);
+        }
+        this.shift.onclick = () => {
+            this.setAttribute('data-shift', this.shift.value);
         }
         this.render();
     }
 
-    disconnectedCallback(){
-        this.button.onclick = null;
+    disconnectedCallback() {
+        this.removeButton.onclick = null;
+        this.waveType.onclick = null;
+        this.invert.onclick = null;
+        this.clamp.onclick = null;
+        this.clampRange.onclick = null;
+        this.amplitude.onclick = null;
+        this.period.onclick = null;
+        this.skew.onclick = null;
+        this.shift.onclick = null;
     }
 
 
-    render(){
-        this.h1.innerHTML = `${exercise}`;
+    render() {
+        // this.h1.innerHTML = `${exercise}`;
     }
 
 }
